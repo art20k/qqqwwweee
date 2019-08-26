267761171fd77631bf67eaa8e47b4bb49413ba56
art20k <41651172+art20k@users.noreply.github.com>
2019-08-27 00:13:13 +0200

    sss

---
+++ src/Asdf.java
@@ -0,0 +1,20 @@
+public class Asdf {
+    int a;
+    int b;
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
